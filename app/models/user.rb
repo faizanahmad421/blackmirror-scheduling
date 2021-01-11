832a50564849a68649ca65f213010e5f12989156
Faizan Ahmad <hafizfaizanahmad421@gmail.com>
2021-01-11 03:53:50 -0800

    User: Added validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  validate :validate_username
 end
